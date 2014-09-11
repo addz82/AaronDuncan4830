07e8e991e00598e958176d5fd3fb9175c3d474df
Aaron Duncan <aaronduncan@mwc-065039.dhcp.missouri.edu>
2014-09-11 15:42:24 -0500

    Initial Commit

---
+++ AaronDuncan4830/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AaronDuncan4830
+//
+//  Created by Aaron Duncan on 9/11/14.
+//  Copyright (c) 2014 Aaron Duncan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AaronDuncan4830/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AaronDuncan4830
+//
+//  Created by Aaron Duncan on 9/11/14.
+//  Copyright (c) 2014 Aaron Duncan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
